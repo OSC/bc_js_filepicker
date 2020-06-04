f26bf1aba127dc09fc3e846fd995da2b7081fe68
msquee <msqueo@osc.edu>
2020-06-04 14:59:58 -0400

    Improve webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -3,14 +3,21 @@ const VueLoaderPlugin = require('vue-loader/lib/plugin');
 
 module.exports = {
   mode: process.env.NODE_ENV || 'production',
-  entry: './src/main.js',
+  entry: path.join(__dirname, '/src/main.js'),
   output: {
     path: path.resolve(__dirname),
     filename: 'form.js'
   },
   module: {
     rules: [
-      { test: /\.js$/, use: { loader: 'babel-loader', options: { presets: ['@babel/preset-env'] } } },
+      { test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
       { test: /\.vue$/, use: 'vue-loader' },
       { test: /\.css$/, use: ['vue-style-loader', 'css-loader']},
     ]
